bc82ed0ebadb1f4cb0bcdd712c4fd876a24208ad
Stanisław Małolepszy <sta@malolepszy.org>
2022-04-20 20:00:50 +0200

    sloik.png

--- src/scenes/sce_stage.ts
+++ src/scenes/sce_stage.ts
@@ -26,37 +26,38 @@ export function scene_stage(game: Game) {
     // Garnek1.
     instantiate(game, [
         transform2d([-6, 0], 0, [4, 3]),
-        render2d([4, 2.33], [0, 0]),
+        render2d([4, 2.66], [0, 0]),
         order(1),
-        grabbable(),
         collide2d(false, 2),
         rigid_body2d(RigidKind.Static, 1.5),
+        grabbable(),
     ]);
 
     // Garnek2.
     instantiate(game, [
         transform2d([0, -3], 0, [4, 3]),
-        render2d([4, 2.33], [0, 1]),
+        render2d([4, 2.66], [0, 1]),
         order(1),
-        grabbable(),
         collide2d(false, 2),
         rigid_body2d(RigidKind.Static, 1.5),
+        grabbable(),
     ]);
 
-    for (let i = 0; i < 5; i += 0.5) {
+    for (let i = 0; i < 5; i += 1) {
         instantiate(game, [
-            transform2d([i + 4, i], 0),
-            render2d([16, 7], [0, 6], hsva_to_vec4(float(0.1, 0.2), 0.2, 1, 1)),
-            collide2d(false, 0.5),
+            transform2d([i + 4, i], 0, [2, 2]),
+            render2d([8, 4], [1, 3]),
+            collide2d(false, 1.3),
             rigid_body2d(RigidKind.Static, 1.5),
+            grabbable(),
         ]);
     }
 
-    let dynamic_count = 20_000;
+    let dynamic_count = 5_000;
     for (let i = 0; i < dynamic_count; i++) {
         instantiate(game, [
             transform2d([float(-8, -4), float(10, 100)], 0),
-            render2d([16, 7], [0, 6], hsva_to_vec4(float(0.1, 0.2), 0.2, 1, 1)),
+            render2d([16, 8], [0, 6], hsva_to_vec4(float(0.1, 0.2), 0.2, 1, 1)),
             // Place entities from closest to the farthest away to avoid overdraw.
             order(1 - i / dynamic_count),
             collide2d(true, 1),
@@ -65,7 +66,7 @@ export function scene_stage(game: Game) {
 
         instantiate(game, [
             transform2d([float(4, 8), float(10, 100)], 0),
-            render2d([16, 7], [1, 6], hsva_to_vec4(float(0.1, 0.2), 0.5, 1, 1)),
+            render2d([16, 8], [1, 6], hsva_to_vec4(float(0.1, 0.2), 0.5, 1, 1)),
             // Place entities from closest to the farthest away to avoid overdraw.
             order(1 - i / dynamic_count),
             collide2d(true, 1),
