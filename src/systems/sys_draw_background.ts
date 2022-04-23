6e728829a9c55460e0c8031fc9f31b60299c40e1
Stanisław Małolepszy <sta@malolepszy.org>
2022-04-23 19:15:46 +0200

    Scale and translate the background

--- src/systems/sys_draw_background.ts
+++ src/systems/sys_draw_background.ts
@@ -1,19 +1,34 @@
-import {Game} from "../game.js";
+import {CameraKind} from "../components/com_camera.js";
+import {BASE_UNIT_SIZE, Game} from "../game.js";
 
 export function sys_draw_background(game: Game, delta: number) {
     let ctx = game.Context2D;
+    let camera_entity = game.Cameras[0];
+    let camera = game.World.Camera[camera_entity];
+    if (camera.Kind === CameraKind.Xr) {
+        throw new Error("XR not implemented");
+    }
 
     ctx.resetTransform();
-    ctx.fillStyle = "#FFD6D5";
+    ctx.fillStyle = "#D7AAA9";
     ctx.fillRect(0, 0, game.ViewportWidth, game.ViewportHeight);
 
-    ctx.setTransform(1, -0.2, -0.3, 1, 0, 0);
-    ctx.fillStyle = "#FFAA79";
-    ctx.fillRect(200, 200, 400, 400);
+    let x = game.ViewportWidth / 2 - camera.Position[0] * game.UnitSize;
+    let y = game.ViewportHeight / 2 + camera.Position[1] * game.UnitSize;
+    let s = game.UnitSize / BASE_UNIT_SIZE;
+    let w = game.ViewportWidth / 2;
+    let h = game.ViewportHeight / 2;
 
-    ctx.setTransform(1, 0.3, 0.2, 1, 0, 0);
+    ctx.fillStyle = "#FFD6D5";
+    ctx.fillRect(x - w, y - h, w * 2, h * 2);
+
+    ctx.setTransform(s, s * -0.2, s * -0.3, s, x, y);
+    ctx.fillStyle = "#FFAA79";
+    ctx.fillRect(0, 0, 400, 400);
+
+    ctx.setTransform(s, s * 0.3, s * 0.2, s, x, y);
     ctx.fillStyle = "#D4FCA9";
     ctx.beginPath();
-    ctx.arc(400, 400, 200, 0, 2 * Math.PI);
+    ctx.arc(0, 0, 200, 0, 2 * Math.PI);
     ctx.fill();
 }
