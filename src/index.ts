010cf4983848836e0a38451cd2b627da17d19be9
Stanisław Małolepszy <sta@malolepszy.org>
2022-04-26 00:38:52 +0200

    sim object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as dat from "dat.gui";
 import {create_texture_from, fetch_image} from "../common/texture.js";
+import {destroy_all} from "./components/com_children.js";
 import {Game} from "./game.js";
 import {scene_stage} from "./scenes/sce_stage.js";
+import {Has} from "./world.js";
 
 let game = new Game();
 
@@ -18,12 +20,32 @@ async function load_texture(game: Game, name: string) {
     game.Textures[name] = create_texture_from(game.Gl, image);
 }
 
+let sim = {
+    pause() {
+        game.Stop();
+    },
+
+    resume() {
+        game.Start();
+    },
+
+    restart() {
+        game.ItemCount = 0;
+        for (let ent = 0; ent < game.World.Signature.length; ent++) {
+            if (game.World.Signature[ent] & Has.ControlProcess) {
+                destroy_all(game.World, ent);
+            }
+        }
+    },
+};
+
 let gui = new dat.GUI();
 
 let gui_sim = gui.addFolder("Simulation");
-gui_sim.add(game, "Stop").name("Pause");
-gui_sim.add(game, "Start").name("Resume");
+gui_sim.add(sim, "restart").name("Restart");
+gui_sim.add(sim, "pause").name("Pause");
+gui_sim.add(sim, "resume").name("Resume");
 
-let gui_items = gui.addFolder("Ingredients");
-gui_items.add(game, "spawnInterval", 0, 1).step(0.01).name("Spawn interval (s)");
-gui_items.add(game, "spawnCount", 0, 5).step(1).name("Spawn count");
+let gui_spawn = gui.addFolder("Spawning");
+gui_spawn.add(game, "spawnInterval", 0, 1).step(0.01).name("Interval (s)");
+gui_spawn.add(game, "spawnCount", 0, 5).step(1).name("Multiplier");
